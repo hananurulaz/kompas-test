2f521c1c92adc0ed9d01bd18561aaa0ffacd63df
Hana Nurul A <h.nurul.a@gmail.com>
2024-01-25 08:40:53 +0700

    first commit

---
+++ Object Repository/Login/btn_Profile.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Profile</name>
+   <tag></tag>
+   <elementGuidId>b0f10181-534e-4fbf-b7e3-71622d957c5d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.sso__link.sso__link__icon.js-sso-user</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Koin'])[1]/following::a[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>6ac68fc3-040a-46d2-85f1-3235dce171cf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sso__link sso__link__icon js-sso-user</value>
+      <webElementGuid>f318652c-8cba-4065-8d48-5959d9c750d0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>javascript:void(0)</value>
+      <webElementGuid>bfa13a4d-38f1-498f-b5eb-ae4e151b1883</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;js csstransitions&quot;]/body[@class=&quot;revampV2&quot;]/div[@class=&quot;wrap&quot;]/div[@class=&quot;header header--sumpah-pemuda clearfix&quot;]/div[@class=&quot;row clearfix header__wrap&quot;]/div[@class=&quot;container clearfix header__row&quot;]/div[@class=&quot;row col-offset-fluid clearfix&quot;]/div[@class=&quot;col-bs10-5 clearfix&quot;]/div[@class=&quot;sso__header col-bs12-4&quot;]/ul[@class=&quot;sso__wrap&quot;]/li[@class=&quot;sso__item&quot;]/a[@class=&quot;sso__link sso__link__icon js-sso-user&quot;]</value>
+      <webElementGuid>f41f5934-3546-4e9b-b193-09fb3198911c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Koin'])[1]/following::a[1]</value>
+      <webElementGuid>c03a5db4-18f4-475c-a7fb-e080f49c0afc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Mode Terang'])[1]/following::a[2]</value>
+      <webElementGuid>b33075e3-4896-4bdc-bbfb-573819b0e60f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login'])[1]/preceding::a[1]</value>
+      <webElementGuid>9c33451f-bb03-48e8-b1a3-40b1222b1302</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Gabung Kompas.com+'])[1]/preceding::a[2]</value>
+      <webElementGuid>2b98466c-c105-404e-b35b-f6371ed34276</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'javascript:void(0)')]</value>
+      <webElementGuid>80dcf9f4-23bf-4e20-a328-d5df7d84c282</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li/a</value>
+      <webElementGuid>a8b74927-627b-4f84-b4a5-413f8fecaaca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'javascript:void(0)']</value>
+      <webElementGuid>fdceb56d-2ed6-4789-a455-4f44a8928dc3</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Login/lbl_LoginForVerify.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lbl_LoginForVerify</name>
+   <tag></tag>
+   <elementGuidId>4a5530ab-447f-4866-a903-4bd0b25e8143</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>p.custom_text</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='KG Media ID'])[1]/following::p[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>7ca151b4-f6d3-4465-ac49-ea0f51bae7a8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>custom_text</value>
+      <webElementGuid>2f4088f4-d776-428e-8625-13cbc3964d4d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Login dengan KG Media ID untuk menggunakan layanan-layanan dari KG Media.</value>
+      <webElementGuid>a6c5ba22-1216-4e29-9dca-a0b1a7e997d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;container&quot;]/div[@class=&quot;block-top&quot;]/p[@class=&quot;custom_text&quot;]</value>
+      <webElementGuid>feea22f6-a9ab-40b8-83f5-2b190faee909</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KG Media ID'])[1]/following::p[1]</value>
+      <webElementGuid>85712822-3089-43e2-bac2-753b202a378d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='KG Media ID'])[2]/preceding::p[1]</value>
+      <webElementGuid>72d92e0b-2d02-4d2e-ab64-5996c547d1ad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/preceding::p[1]</value>
+      <webElementGuid>cd490126-5682-48ce-8b55-744eca565b68</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Login dengan KG Media ID untuk menggunakan layanan-layanan dari KG Media.']/parent::*</value>
+      <webElementGuid>1e5f16cc-2c44-411d-8b89-f446e2327baf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//p</value>
+      <webElementGuid>52752b66-aefe-4f2c-a8fb-e2ba401b04d6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = 'Login dengan KG Media ID untuk menggunakan layanan-layanan dari KG Media.' or . = 'Login dengan KG Media ID untuk menggunakan layanan-layanan dari KG Media.')]</value>
+      <webElementGuid>affb355f-e56c-48a4-8597-c97057be05e0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
