6fa30350aa4aeac615c7e4d03845121cb70438a0
hana <h.nurul.a@gmail.com>
2024-01-25 20:22:03 +0700

    search news

---
+++ Object Repository/NewsSearch/lbl_newsTitle.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lbl_newsTitle</name>
+   <tag></tag>
+   <elementGuidId>d7a745a7-e21b-4fde-8d0d-6701bb8d6e7e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h1.read__title</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='TikTok Shop Dilarang'])[1]/following::h1[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h1</value>
+      <webElementGuid>5a6eac3b-d1de-416f-a6c7-c8e393ef3fa4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>read__title</value>
+      <webElementGuid>a809d2cf-8a47-4643-81db-eef03226b78b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>TikTok Shop Ditutup Hari Ini, &quot;Seller&quot; Diminta Beralih ke &quot;E-commerce&quot;</value>
+      <webElementGuid>634167bb-4a94-46ee-84cd-9ec53182e84e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;js csstransitions&quot;]/body[@class=&quot;theme--money page--read&quot;]/div[@class=&quot;wrap&quot;]/div[@class=&quot;container clearfix&quot;]/div[@class=&quot;row mt2 col-offset-fluid clearfix&quot;]/div[@class=&quot;col-bs10-10&quot;]/h1[@class=&quot;read__title&quot;]</value>
+      <webElementGuid>e3f874b8-c570-4680-803f-a3c4188b0565</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TikTok Shop Dilarang'])[1]/following::h1[1]</value>
+      <webElementGuid>a925c0c7-0f7b-4342-9826-8b03e2ac1186</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='TikTok Shop Terlarang'])[1]/following::h1[1]</value>
+      <webElementGuid>e2f83093-9331-4fe6-9fd8-d359ebf745a0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kompas.com'])[2]/preceding::h1[1]</value>
+      <webElementGuid>e7170190-f190-48d7-8eab-2026b225660a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h1</value>
+      <webElementGuid>3f8d35ba-f021-41ad-940d-07feef127c19</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h1[(text() = 'TikTok Shop Ditutup Hari Ini, &quot;Seller&quot; Diminta Beralih ke &quot;E-commerce&quot;' or . = 'TikTok Shop Ditutup Hari Ini, &quot;Seller&quot; Diminta Beralih ke &quot;E-commerce&quot;')]</value>
+      <webElementGuid>86eebed1-4ecd-4866-9289-ccec6d5e2bd3</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
